4d36fb76cd184c38d953196b295671b81b54dfd3
johannfjs <johannfjs@gmail.com>
2015-01-30 09:28:22 -0500

    Clase 04

---
+++ Modulo 1/Clase 04/Sesion04_1/app/src/main/java/area51/com/sesion04/MainActivity.java
@@ -0,0 +1,91 @@
+package area51.com.sesion04;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+import area51.com.adapters.ListaAdapter;
+import area51.com.models.Persona;
+import area51.com.utils.Constant;
+
+
+public class MainActivity extends ActionBarActivity {
+    ListaAdapter adapter;
+    ListView lvLista;
+    EditText txtNombre;
+    EditText txtApellidoPaterno;
+    EditText txtApellidoMaterno;
+    EditText txtEdad;
+    Spinner spSexo;
+    Button btnGrabar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvLista = (ListView) findViewById(R.id.lvLista);
+        txtNombre = (EditText) findViewById(R.id.txtNombre);
+        txtApellidoPaterno = (EditText) findViewById(R.id.txtApellidoPaterno);
+        txtApellidoMaterno = (EditText) findViewById(R.id.txtApellidoMaterno);
+        txtEdad = (EditText) findViewById(R.id.txtEdad);
+        spSexo = (Spinner) findViewById(R.id.spSexo);
+        btnGrabar = (Button) findViewById(R.id.btnGrabar);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Constant.LISTA_PERSONA == null)
+            Constant.LISTA_PERSONA = new ArrayList<Persona>();
+
+        //Constant.LISTA_PERSONA.add(new Persona(1, "Nombre", "ApellidoPaterno", "ApellidoMaterno", 30, "Masculino"));
+
+        adapter = new ListaAdapter(getApplicationContext());
+        lvLista.setAdapter(adapter);
+
+        btnGrabar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Constant.LISTA_PERSONA.add(new Persona(
+                        Constant.LISTA_PERSONA.size(),
+                        txtNombre.getText().toString(),
+                        txtApellidoPaterno.getText().toString(),
+                        txtApellidoMaterno.getText().toString(),
+                        Integer.parseInt(txtEdad.getText().toString()),
+                        spSexo.getSelectedItem().toString()));
+                adapter.notifyDataSetChanged();
+                
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Modulo 1/Clase 04/Sesion04_1/app/src/main/java/area51/com/utils/Constant.java
@@ -0,0 +1,12 @@
+package area51.com.utils;
+
+import java.util.ArrayList;
+
+import area51.com.models.Persona;
+
+/**
+ * Created by Johann on 30/01/2015.
+ */
+public class Constant {
+    public static ArrayList<Persona> LISTA_PERSONA = new ArrayList<Persona>();
+}
